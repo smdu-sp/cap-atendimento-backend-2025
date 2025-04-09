2b9b34da78c3438f18bb145164cca71c1bafaeee
Fernando Lacerda <anjosdelacerda@gmail.com>
2025-04-09 13:38:30 -0300

    fix/comentando rota /hoje no agendamento controller, porque agenndaDiaria não esta em agendamento.service. Modificando a importação do SGU para que o this.connectfuncione

--- src/prisma/sgu.service.ts
+++ src/prisma/sgu.service.ts
@@ -1,5 +1,6 @@
 import { Global, Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/sgu/client';
+// import { PrismaClient } from '@prisma/sgu/client';
+import { PrismaClient } from '@prisma/client';
 
 @Global()
 @Injectable()
